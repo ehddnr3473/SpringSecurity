8d78945adf693914aa1d061fe5c0ce7246c6fbfd
김동욱 <ehddnr73@naver.com>
2025-11-03 11:41:18 +0900

    기본 인가

---
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers("/", "login").permitAll()
+                                .requestMatchers("/admin").hasRole("ADMIN") // 로그인 및 ADMIN Role 소유
+                                .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
+                                .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
